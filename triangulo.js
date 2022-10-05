1883cfff62b3322f7343853729ba63b0bf2d825f
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-10-05 15:17:29 -0300

    Add files via upload
    8) Escreva um algoritmo para ler as dimensões de um triângulo (base e altura), calcular e escrever a área do triângulo. Dica a=b.h/2

---
+++ triangulo.js
@@ -0,0 +1,6 @@
+function multiplique(){
+    var numero1=prompt(("digite o valor da base do triângulo:"));
+    var numero2=(prompt("digite o valor da altura do triângulo:"));
+    var multiplique=numero1*numero2/ 2;
+    alert("A área do triângulo é :"+multiplique);
+    }
